edd65a69b4eea982959ebd7b102e0c063aa47494
Ahmed Kamal <ahmd675asd@gmail.com>
2024-09-14 23:40:09 +0300

    3 Components Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "@/Styles/App.css";
 import { Outlet } from "react-router-dom";
+import Header from "@/Components/Header";
+import Footer from "@/Components/Footer";
+import Sale from "@/Components/Sale";
 
 function App() {
   return (
     <>
+      <Sale />
+      <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
